e46b45a07307bfb12321b78cc93a528483a01723
MateoBenkovic <mateo.benkovic648@gmail.com>
2024-11-05 15:30:05 +0100

    third commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("jh");
         System.out.println("sdf");
-        System.out.println(kjasdfh);
+        System.out.println("kjasdfh");
         }
 }
