863f180626bafa61a5de732abd44026054cbc7f9
MateoBenkovic <mateo.benkovic648@gmail.com>
2024-11-05 15:29:46 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("jh");
         System.out.println("sdf");
+        System.out.println(kjasdfh);
         }
 }
